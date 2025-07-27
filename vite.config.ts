57638c60fcf540bac1c2cd7d3c76a19a0051f30a
Jada Mathele <jadamaadimo.mathele@gmail.com>
2025-07-27 17:44:32 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,28 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name?.endsWith('.css')) {
+            return 'assets/[name]-[hash].css'
+          }
+          return 'assets/[name]-[hash].[ext]'
+        }
+      }
+    },
+    target: 'es2015',
+    minify: 'esbuild'
+  },
+  server: {
+    fs: {
+      strict: false
+    }
+  }
 })
